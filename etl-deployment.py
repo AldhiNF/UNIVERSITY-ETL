e2909b9b18a5ce8f6d95e51c82cb8c8496193f71
Aldhinf <mellaldhi@gmail.com>
2025-07-27 20:48:30 +0700

    feat: restructure deployment and add final notebook analysis

---
+++ etl-deployment.py
@@ -0,0 +1,16 @@
+from prefect import flow
+from prefect_github.repository import GitHubRepository
+
+# Load GitHub repository block yang sudah kamu buat di Prefect Cloud
+github_repository_block = GitHubRepository.load("github-repository-univ")
+
+# Deploy flow dari repositori GitHub
+flow.from_source(
+    source=github_repository_block,
+    entrypoint="etl-main.py:etl_flow",  # nama fungsi di dalam file
+).deploy(
+    name="university-etl",
+    work_pool_name="etl-workers"
+)
+
+print("Flow deployed successfully!")
